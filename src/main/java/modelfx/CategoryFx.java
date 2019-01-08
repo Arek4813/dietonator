07cea3f11c70ca57d0bca15623d97f1d8c5f7f48
uminskiM <marcinuminski1998@gmail.com>
2019-01-08 11:08:29 +0100

    Add package of files associated with category

---
+++ src/main/java/modelfx/CategoryFx.java
@@ -0,0 +1,20 @@
+package modelfx;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class CategoryFx {
+    private StringProperty name = new SimpleStringProperty();
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+}
